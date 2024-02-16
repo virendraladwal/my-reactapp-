b74412cc9c26647743026c482cbf7a6dbe91996c
virendra ladwal <virendraladwal06@gmail.com>
2024-02-16 23:12:18 +0530

    responsive

--- src/Components /Works/works.jsx
+++ src/Components /Works/works.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import './works.css'
-import portfolio1 from '../Assets/work1.jpg'
-import portfolio2 from '../Assets/work2.jpg'
-import portfolio3 from '../Assets/work3.jpg'
-import portfolio4 from '../Assets/work4.jpg'
-import portfolio5 from '../Assets/work5.jpg'
-import portfolio6 from '../Assets/work6.jpg'
+
+import portfolio1 from '../Assets/work1.webp'
+import portfolio2 from '../Assets/work2.webp'
+import portfolio3 from '../Assets/work3.webp'
+import portfolio4 from '../Assets/work4.webp'
+import portfolio5 from '../Assets/work5.webp'
+import portfolio6 from '../Assets/work6.webp'
 
 const Works = () => {
   return (
-    <section id= 'works'>
-        <h2 className='workstitle'>my portfolio</h2>
+    <section id='works'>
+        <h2 className='worksTitle'>My Portfolio</h2>
         <span className='worksDesc'>
-           Briefly introduce Kaka Da Dhaba, highlighting its vision, mission, and values.
-           Describe the concept behind the restaurant, such as its focus on traditional Indian 
-           cuisine with a modern twist or its commitment to using fresh, locally sourced ingredients.
+           Briefly introduce Kaka Da Dhaba,<br/> 
+           highlighting its vision, mission, and values.Describe<br/> 
+           Describe the concept behind the restaurant, such as its focus on traditional Indian <br/> 
+           cuisine with a modern twist or its commitment to using fresh,<br/>  locally sourced ingredients.
         </span>
 
-        <div>
+        <div className='worksImgs'>
          <img src={portfolio1} alt="" className='worksImg' />
          <img src={portfolio2} alt="" className='worksImg' />
          <img src={portfolio3} alt="" className='worksImg' />
